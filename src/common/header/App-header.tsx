74c4c318990f722105f5f625cfb8c5cf2c816578
buhtib <2418531916@qq.com>
2018-11-30 20:00:56 +0800

    完成头部样式

--- src/common/header/App-header.tsx
+++ src/common/header/App-header.tsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 import './App-header.scss'
+import refreshImg from '../../assets/img/refresh.png'
+import emailImg from '../../assets/img/email.png'
+import searchImg from '../../assets/img/seach.png'
 
 export default function AppHeader() {
     return (
         <div id="app_header">
-            <div className="email"></div>
-            <h1 className="title"></h1>
-            <div className="search"></div>
+            <div className="email">
+                <div className="circle"></div>
+                <img src={emailImg} alt=""/>
+            </div>
+            <h1 className="title">
+                <span>今日头条</span>
+                <img src={refreshImg} alt=""/>
+            </h1>
+            <div className="search">
+                <img src={searchImg} alt=""/>
+            </div>
         </div>      
     )
 }
